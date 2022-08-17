59c26fb4d426aca297992527b5ee473f0fd52a86
simonlee91 <simon@provalidator.com>
2022-08-17 09:30:02 +0900

    2 files modified, 6 deleted

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import _ from 'lodash';
 function component() {
   const element = document.createElement('div');
 
-  // Lodash, now imported by this script
+  // 아래 스크립트 실행하기 위해서 lodash import 함
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
   return element;
